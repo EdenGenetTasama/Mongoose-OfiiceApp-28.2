5f035d98e25bb1658ee9607b53d424ac03a22ac2
EdenGenetTasama <edentasama19@gmail.com>
2022-03-03 13:53:47 +0200

    fixs

--- server/server.js
+++ server/server.js
@@ -4,34 +4,35 @@ require('./DB/officeDB')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = 8000;
+const port = 8000
 const employeeRouter = require('./Routes/office-route.js')
-const userRouter = require('./Routes/user-route');
+const userRouter = require('./Routes/user-route')
 const path = require('path')
 
-const passport = require('passport');
-require('./config/passport')(passport);
+const passport = require('passport')
+require('./config/passport')(passport)
 
-
-app.use(cors());
-app.use(express.json({extended : true}));
-app.use(express.urlencoded({extended : true}))
+app.use(cors())
+app.use(express.json({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   console.log(`Server is up at port ${port}`)
 })
 
-
-app.use(passport.initialize());
-app.use('/auth', userRouter);
-app.use('/api',passport.authenticate('jwt', {session:false}) ,employeeRouter);
+app.use(passport.initialize())
+app.use('/auth', userRouter)
+app.use(
+  '/api',
+  passport.authenticate('jwt', { session: false }),
+  employeeRouter
+)
 
 //פונקציה שעוברת לנו לבדוק מה הנתב ש
 
-if(process.env.NODE_ENV==='production'){
-  app.use(express.static('client/build'));
-      app.get('*',(req,res)=>   
-  
-  res.sendFile(path.join(__dirname,'../client/build','index.html')))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join('client/build')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+  )
 }
-
